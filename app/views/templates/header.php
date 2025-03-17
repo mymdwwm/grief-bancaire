3cdb71f097b02fd5b3843f01610eedfc059e46aa
Myriam <myriam.malki@hotmail.fr>
2025-03-17 17:05:57 +0100

    set début projet

---
+++ app/views/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionnaire</title>
+</head>
+<body>
+
+<header>
+<nav>
+    <ul>
+        <li>
+            <a href="">Acceuil</a>
+        </li>
+    </ul>
+</nav>
+</header>
+
+
+
+
